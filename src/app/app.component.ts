54a5c5e8da0b31659ffaf6219ee0839e47a2b84f
Rodolfo Puente Zuñiga <rjunior007@live.com.mx>
2024-05-04 12:03:49 -0600

    1rs save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { VideopipeComponent } from "./videopipe/videopipe.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, VideopipeComponent]
 })
 export class AppComponent {
   title = 'miniproyectoTWeb';
