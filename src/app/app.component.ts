a0cc4612a0107e0eef19c6e385fa4d695efa4496
Meow <al244179@edu.uaa.mx>
2024-05-04 17:05:55 -0600

    info table
    ps puro lorem puro table hasta q sepamos q le ponemos, tengo hambre btw hola diego

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormComponent } from './form/form.component';
+import { InfoComponent } from './info/info.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormComponent],
+  imports: [RouterOutlet, FormComponent, InfoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
