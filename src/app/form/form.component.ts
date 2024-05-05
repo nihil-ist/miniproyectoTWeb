d45ebbd0a82a954ab75787be60bf8fa64cc5578a
Meow <al244179@edu.uaa.mx>
2024-05-05 00:10:32 -0600

    calendario
    mmm pues SEGUN YO ya jala lo de las fechas y las reservaciones para que no se repitan, todo se guarda  a traves de un local storage en un array, la verda no lo cheque tanto jeje

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { from } from 'rxjs';
@@ -5,17 +6,85 @@ import { from } from 'rxjs';
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
+
 export class FormComponent {
 
-imageUrl: string = 'assets/img/house.jpeg';
+  reservation: Reservation = {
+    arrivalDate: null,
+    departureDate: null
+  };
 
-  submitForm(){
+  reservations: Reservation[] = [];
 
-    // console.log(this.from.value);
+  // arrivalDate:string | null = null;
+  // departureDate:string | null = null;
+
+  imageUrl: string = 'assets/img/house.jpeg';
+
+  ngOnInit(){
+    const storedReservations = localStorage.getItem('reservations');
+    if (storedReservations) {
+      this.reservations = JSON.parse(storedReservations);
+    }
+    // this.arrivalDate = localStorage.getItem('selectedDate');
+    // this.departureDate = localStorage.getItem('departureDate');
   }
 
+  submitForm(){
+    // console.log(this.from.value);
+    this.reservations.push(this.reservation);
+    localStorage.setItem('reservations', JSON.stringify(this.reservations));
+  }
+
+  saveDate(event: any) {
+    const selectedDate = new Date(event.target.value);
+    const currentDate = new Date();
+    if (selectedDate < currentDate) {
+      alert("That date has passed already.");
+      event.target.value = '';
+    } else {
+      const isDateAvailable = this.isDateAvailable(selectedDate);
+      if (!isDateAvailable) {
+        alert("This range of date is already reserved, choose another dates.");
+        event.target.value = '';
+      } else {
+        this.reservation.arrivalDate = selectedDate;
+      }
+    }
+  }
+
+  saveDepartureDate(event: any) {
+    const departureDate = new Date(event.target.value);
+    const arrivalDate = this.reservation.arrivalDate;
+    if (arrivalDate && departureDate <= arrivalDate) {
+      alert("Departure date must be after arrival date");
+      event.target.value = '';
+    } else {
+      this.reservation.departureDate = departureDate;
+    }
+  }
+
+  isDateAvailable(selectedDate: Date): boolean {
+    for (const reservation of this.reservations) {
+      if (
+        reservation.arrivalDate &&
+        reservation.departureDate &&
+        selectedDate >= reservation.arrivalDate &&
+        selectedDate <= reservation.departureDate
+      ) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
+}
+
+interface Reservation {
+  arrivalDate: Date | null;
+  departureDate: Date | null;
 }
