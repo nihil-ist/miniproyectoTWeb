f9fb979e7b9bc769bc739370c685a5d157f93dba
Rodolfo Puente Zuñiga <rjunior007@live.com.mx>
2024-05-05 00:12:38 -0600

    contenido del home hecho

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { VideopipeComponent } from "./videopipe/videopipe.component";
+
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, VideopipeComponent]
+    imports: [RouterOutlet]
 })
 export class AppComponent {
   title = 'miniproyectoTWeb';
