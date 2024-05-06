94ddc360ec9bd7dd380f56044abe23df3d2b2ffe
Rodolfo Puente Zuñiga <rjunior007@live.com.mx>
2024-05-06 16:27:54 -0600

    war is over (ya separe los component)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormComponent } from './form/form.component';
 import { InfoComponent } from './info/info.component';
+import { TableComponent } from "./table/table.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, FormComponent, InfoComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, FormComponent, InfoComponent, TableComponent]
 })
 export class AppComponent {
   title = 'miniproyectoTWeb';
