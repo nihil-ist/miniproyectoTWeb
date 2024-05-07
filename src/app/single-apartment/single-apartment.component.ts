4558aea2626fc38f42b35947ad02ed3af1a079dc
CocoAsombroso <Ivanelmen420@gmail.com>
2024-05-06 19:40:02 -0600

    El acceso al form ya esta desde la parte de apartamento
    Este se abre en un modal

--- src/app/single-apartment/single-apartment.component.ts
+++ src/app/single-apartment/single-apartment.component.ts
@@ -2,11 +2,12 @@ import { Component,Input } from '@angular/core';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { Apartment } from '../interfaces/apartment';
 import { ApartmentsService } from '../apartmentsService/apartments.service';
+import { FormComponent } from '../form/form.component';
 
 @Component({
   selector: 'app-single-apartment',
   standalone: true,
-  imports: [],
+  imports: [FormComponent],
   templateUrl: './single-apartment.component.html',
   styleUrl: './single-apartment.component.css'
 })
